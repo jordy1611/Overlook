eba44a23fc731d79329afdba139be33b4127c8fb
Jordy Shryock <jordy1611@gmail.com>
2020-07-30 13:00:28 -0600

    Initialize login functionality, can tell if login is user or manager

--- src/index.js
+++ src/index.js
@@ -12,30 +12,51 @@ import BookingRepo from './Booking-repo';
 import TodayBookingRepo from './TodayBooking-repo';
 import UserBookingRepo from './UserBooking-repo';
 import Manager from './Manager';
+// import loginUser from './loginUser'
 import deleteBooking from './DeleteBooking';
 import domUpdates from './DomUpdates';
 import fetchAllData from './FetchAllData';
 import postNewBooking from './PostNewBooking';
 
+let user, manager, customer;
 window.addEventListener('load', onLoad);
 window.addEventListener('click', clickHandler);
 
-function onLoad() {}
+function onLoad() {
+  user = new User();
+}
 function clickHandler(event) {
   if (event.target.classList.contains('login-button')) {
     event.preventDefault();
     loginUser();
+    console.log(manager)
+    console.log(customer)
   }
 }
 
 function loginUser() {
-  console.log('ayyyyyyy')
+  const userName = document.querySelector('.username-input').value
+  const password = document.querySelector('.password-input').value
+  console.log(password)
+  login(userName, password);
 }
 
-function validatePassword() {
-
+function login(userName, password) {
+  if (userName === 'manager' && password === 'overlook2020') {
+    loginAsManager()
+  } else if (userName.slice(0, 8) === 'customer' && password === 'overlook2020') {
+    loginAsCustomer()
+  } else {
+    alert('We are terribley sorry to tell you that either the username or password entered is incorrect.')
+  }
 }
 
-function createCurrentUser() {
+function loginAsManager() {
+  manager = 'manager'
+  // new Manager
+}
 
+function loginAsCustomer() {
+  customer = 'customer'
+  // new Customer
 }
