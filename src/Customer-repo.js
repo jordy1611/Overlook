354690663514a629d95929338dfd07a7cdf0a0a6
Jordy Shryock <jordy1611@gmail.com>
2020-07-30 13:32:27 -0600

    Create CustomerRepo file and first test

--- src/Customer-repo.js
+++ src/Customer-repo.js
@@ -1,4 +1,12 @@
+// import Customer from '../src/Customer'
 /*Array of user objects
 one property which is users
 method return current user based on id
 return user based on name*/
+class CustomerRepo {
+  constructor(customers) {
+    this.customers = customers
+  }
+}
+
+export default CustomerRepo
--- test/Customer-repo-test.js
+++ test/Customer-repo-test.js
@@ -8,8 +8,8 @@ import sampleUserData from './sampleData/sampleUsers'
 // import Repo from '../src/Repo'
 // import RoomRepo from '../src/Room-repo'
 // import Room from '../src/Room'
-// import CustomerRepo from '../src/Customer-repo'
-// import Customer from '../src/Customer'
+import CustomerRepo from '../src/Customer-repo'
+import Customer from '../src/Customer'
 // import TodayBookingRepo from '../src/TodayBooking-repo';
 // import UserBookingRepo from '../src/UserBooking-repo';
 // import User from '../src/User'
@@ -17,13 +17,21 @@ import sampleUserData from './sampleData/sampleUsers'
 
 //delete unused imports
 
-describe('thisTest', () => {
+describe('CustomerRepo', () => {
+  let customer0, customer1, customer2, customer3, customer4, customers,
+  customerRepo;
+  before(() => {
+    customer0 = new Customer(sampleUserData[0]);
+    customer1 = new Customer(sampleUserData[1]);
+    customer2 = new Customer(sampleUserData[2]);
+    customer3 = new Customer(sampleUserData[3]);
+    customer4 = new Customer(sampleUserData[4]);
+    customers = [customer0, customer1, customer2, customer3, customer4];
+    customerRepo = new CustomerRepo(customers)
+  });
 
-  // before(() => {
-  //
-  // })
-
-  it('should be a test', () => {
-    expect('test').to.eql('test');
+  it('should have an array of customer objects', () => {
+    expect(customerRepo.customers.length).to.eql(5);
+    expect(customerRepo.customers[0].name).to.eql('Leroy Jenkins');
   })
 })
