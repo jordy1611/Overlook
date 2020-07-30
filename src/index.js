e7f6230d9a481bfc733dc4da6a954ecf6d5f7626
Jordy Shryock <jordy1611@gmail.com>
2020-07-29 21:24:25 -0600

    Begin login page css structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+// import './css/_mixins.scss';
+// import './css/_login-form.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
