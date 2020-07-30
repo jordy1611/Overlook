d314aeadaf76cc6ff4e329bbafda6856769301f9
Jordy Shryock <jordy1611@gmail.com>
2020-07-29 18:00:18 -0600

    Create sample testing data

---
+++ test/sampleData/constSampleBookings.js
@@ -0,0 +1,32 @@
+const constSampleBookingData = [
+  {"id":"5fwrgu4i7k55hl6sz","userID":1,"date":"2020/07/22","roomNumber":15,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6t5","userID":1,"date":"2020/08/09","roomNumber":24,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6t6","userID":1,"date":"2020/01/10","roomNumber":12,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6t7","userID":1,"date":"2020/02/16","roomNumber":7,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6t8","userID":1,"date":"2020/02/05","roomNumber":12,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6t9","userID":2,"date":"2020/02/14","roomNumber":14,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6ta","userID":2,"date":"2020/01/11","roomNumber":9,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tb","userID":2,"date":"2020/02/06","roomNumber":5,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tc","userID":2,"date":"2020/01/30","roomNumber":13,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6td","userID":2,"date":"2020/01/31","roomNumber":20,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6te","userID":2,"date":"2020/01/19","roomNumber":8,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tf","userID":3,"date":"2020/01/25","roomNumber":2,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tg","userID":3,"date":"2020/02/03","roomNumber":17,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6th","userID":3,"date":"2020/02/26","roomNumber":15,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6ti","userID":10,"date":"2020/01/22","roomNumber":11,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tj","userID":10,"date":"2020/01/17","roomNumber":7,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tk","userID":10,"date":"2020/09/11","roomNumber":20,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tl","userID":10,"date":"2020/09/10","roomNumber":8,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tm","userID":10,"date":"2020/12/16","roomNumber":19,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tn","userID":23,"date":"2020/08/17","roomNumber":5,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6to","userID":23,"date":"2020/02/22","roomNumber":13,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tp","userID":23,"date":"2020/01/23","roomNumber":22,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tq","userID":23,"date":"2020/02/03","roomNumber":12,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tr","userID":19,"date":"2020/01/24","roomNumber":4,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6ts","userID":200,"date":"2020/01/26","roomNumber":7,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tt","userID":3,"date":"2020/11/16","roomNumber":5,"roomServiceCharges":[]},
+  {"id":"5fwrgu4i7k55hl6tu","userID":3,"date":"2021/01/28","roomNumber":6,"roomServiceCharges":[]}
+]
+
+
+export default constSampleBookingData
---
+++ test/sampleData/sampleUsers.js
@@ -0,0 +1,9 @@
+const sampleUserData = [
+  {"id":1,"name":"Leroy Jenkins"},
+  {"id":2,"name":"Chip Skylark"},
+  {"id":3,"name":"Marty McFly"},
+  {"id":10,"name":"Bruce Wayne"},
+  {"id":23,"name":"Michael Jordan"}
+]
+
+export default sampleUserData
