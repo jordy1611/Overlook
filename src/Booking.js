5bebf552c5337ce8aacdc0fb9c6c406da62b6502
Jordy Shryock <jordy1611@gmail.com>
2020-07-30 21:17:54 -0600

    Create booking class and test

--- src/Booking.js
+++ src/Booking.js
@@ -1,2 +1,15 @@
 /*Individual bookings
 properties, id(wtf), userID, date, room number, roomservicecharges*/
+class Booking {
+  constructor({id, userID, date, roomNumber, roomServiceCharges}) {
+    this.id = id;
+    this.userID = userID;
+    this.date = date;
+    this.roomNumber = roomNumber;
+    this.roomServiceCharges = roomServiceCharges;
+  }
+}
+
+
+export default Booking
+// {"id":"5fwrgu4i7k55hl6sz","userID":9,"date":"2020/04/22","roomNumber":15,"roomServiceCharges":[]}
