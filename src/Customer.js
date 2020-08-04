2ce45287d7c6cfcb6aa15a70efc068b6bbf506b8
Jordy Shryock <jordy1611@gmail.com>
2020-08-04 17:57:50 -0600

    Remove unused comments from testing files

--- src/Customer.js
+++ src/Customer.js
@@ -1,9 +1,5 @@
 import User from '../src/User'
-/*only 1
-porperty isManager = true (for if statements?)
-methods return todays revenue
-methods search user repo by user name, delete booking
-*/
+
 class Customer extends User{
   constructor({id, name}) {
     super(id, name);
